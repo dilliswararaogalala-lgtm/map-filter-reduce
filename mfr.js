7dc9a35a1c4b3a9a14d86ddf000d8a78ea4d49d4
Dilliswararao Galala <dilliswararaogalala@Dilliswararaos-MacBook-Pro.local>
2025-11-09 16:03:22 +0530

    solved one more problem

--- mfr.js
+++ mfr.js
@@ -9,6 +9,10 @@ const isItRepeat = (lists, element) => {
 
 const add = (x, y) => x + y;
 
+const isDoInGroup = (musicNotes) => musicNotes === 'do';
+
+const isItBelow32 = temperature => temperature < 32
+
 const countOfRibbons = ["red", "blue", "red", "green", "red", "blue"].reduce(festivalRibbonCount, 0);
 
 const starGlazingLog = [["Orion", "Leo"], ["Taurus"], ["Orion", "Gemini"]].flat();
@@ -21,4 +25,12 @@ const AttendanceList = [["Asha", "Ravi", "Neel"], ["Ravi"], ["Asha", "Meera"]]
 
 const noOfCandies = [[5, 3], [2], [4, 1]]
   .flat()
-  .reduce(add, 0)
+  .reduce(add, 0);
+
+const isDoInGrp = [["mi", "fa", "so"], ["do", "mi"], ["fa"]]
+  .flat()
+  .some(isDoInGroup)
+
+const isTempBelow32 = [[22, 23],[25, 24, 22],[29]]
+  .flat()
+  .some(isItBelow32)
