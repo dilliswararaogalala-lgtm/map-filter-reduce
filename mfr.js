f118fd188e78c75bca28646e2dd1b88577722340
Dilliswararao Galala <dilliswararaogalala@Dilliswararaos-MacBook-Pro.local>
2025-11-09 14:25:38 +0530

    creted a test frame frame work in main program

--- mfr.js
+++ mfr.js
@@ -1,3 +1,31 @@
-const favouriteRibbonCount = (count, ribbonColor) => ribbonColor === 'blue' ? count + 1 : count;
+const festivalRibbonCount = (count, ribbonColor) => ribbonColor === 'blue' ? count + 1 : count;
 
-console.log(["red", "blue", "red", "green", "red", "blue"].reduce(favouriteRibbonCount, 0));
+function createMessage(descrip, symbol, expected, exactOutput) {
+  let dataPoints = `\n=> expected:\n${expected}\n`;
+  dataPoints += `=> exact output:\n${exactOutput} \n`;
+  const withOutDataPoints = `${symbol}| ${descrip}`;
+  const withDataPoints = `${symbol}| ${descrip}${dataPoints}`;
+  const message = symbol === "✅" ? withOutDataPoints : withDataPoints;
+  return message;
+}
+
+const getSymbol = (expected, exactOutput) => expected === exactOutput ? "✅" : "❌";
+
+function testTyering(discrip, operation, data, expected) {
+  const exactOutput = data.reduce(festivalRibbonCount, 0)
+  const symbol = getSymbol(expected, exactOutput);
+  const message = createMessage(discrip, symbol, expected, exactOutput);
+  console.log(message);
+}
+
+function repeatChar(char, noOfTimes) {
+  return char.repeat(noOfTimes);
+}
+
+function underline(string) {
+  return `\n${string}\n${repeatChar("-", string.length)}\n`
+}
+
+console.log(underline("FESTIVAL RIBBON COUNT"));
+testTyering('ribbons have cut', festivalRibbonCount, ["red", "blue", "red", "green", "red", "blue"], 2);
+testTyering('ribbons have cut', festivalRibbonCount, ["red", "red", "green", "red"], 0);
